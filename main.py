a57e502a03f937778885c17d52b9301f225c1af0
Serhii <casiomania@proton.me>
2024-03-09 19:55:31 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Lesson 08 / Homework
+
+# Important: do not use loops to implement the main logic.
+# You must use recursion.
+# The loop can be used only in 4 tasks to find the sum of numbers.
+#
+# Task 1.
+# Write a recursive function for finding the degree of a number.
+# Task 2.
+# Write a recursive function that displays N stars in a row, the number N is given by the user.
+# Illustrate the function with an example. (Test)
+# Task 3.
+# Write a recursive function that calculates the sum of all numbers in the range from a to b.
+# The user enters a and b. Illustrate the operation of the function with an example.
+# Optional:
+# Task 4.
+# Write a recursive function that takes a one-dimensional array of 100 randomly filled integers
+# and finds the position from which the sequence of 10 numbers begins, whose sum is minimal.
